968bcb012e9d00081522589601370093d289d9b6
Implantac <166657907+Implantac@users.noreply.github.com>
2025-03-04 17:42:49 -0300

    Add ERP_USE_Web/src/swagger.js 1741120968851

---
+++ ERP_USE_Web/src/swagger.js
@@ -0,0 +1,26 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'ERP USE Web API',
+      version: '1.0.0',
+      description: 'API Documentation for ERP USE Web System',
+      contact: {
+        name: 'Claudinei Silva',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'], // Path to the API routes
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = swaggerSpec;
